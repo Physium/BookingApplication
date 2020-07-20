01a7ba77ca50f7e7aadf049467bf708bb25b7edd
Wei Jun Loh <wjloh91@gmail.com>
2020-07-21 01:11:41 +0800

    added validations accross all models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,28 @@
 class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  validate :check_time_slot, :check_time, :check_date
+
+  # check for booking overlaps
+  def check_time_slot
+    overlap_meetings = Booking.all.where("room_id = ? AND start_time <= ? AND end_time >= ?",room_id, start_time, end_time).count
+
+    if overlap_meetings > 0
+      errors[:base] << "Current Booking Overlap with Other Bookings"
+    end
+  end
+
+  def check_time
+    if start_time > end_time
+      errors.add(:start_time, "is Greater Than or Equal to End Time")
+    end
+  end
+
+  def check_date
+    if start_time.to_date != end_time.to_date
+      errors[:base] <<  "Keep Bookings to a Day Only!"
+    end
+  end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
     has_many :bookings, dependent: :destroy
     has_many :users, through: :bookings
+
+    validates :name, presence: true, 
+                        length: { minimum: 3, maximum: 25 },
+                        uniqueness: { case_sensitive: false }
 end
