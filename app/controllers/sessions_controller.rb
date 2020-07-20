c72d2f3b00181b3fd6d075694718ecd9d4d10467
Wei Jun Loh <wjloh91@gmail.com>
2020-07-20 22:41:56 +0800

    added simple authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
+  
   def new
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
-  def login
+  #use this as a login page
+  def page_requires_login
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'welcome/index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'authorized', to: 'sessions#page_requires_login'
+
   resources :bookings
   resources :rooms
   resources :users
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
