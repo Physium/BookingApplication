a40ea86b9339d97d1dcafdb8017706a427078feb
Wei Jun Loh <wjloh91@gmail.com>
2020-07-24 04:34:27 +0800

    update minor booking datetimepicker config and some test data

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -4,4 +4,9 @@ class BookingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save without title" do
+    booking = Booking.new
+    assert_not booking.save, "Saved the booking without a title"
+  end
 end
