07c933c0107519e4a8d830e14fdb48b33f85eb0d
Wei Jun Loh <wjloh91@gmail.com>
2020-07-24 15:28:51 +0800

    updated fixtures variables and included booking model test

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -5,14 +5,25 @@ class BookingTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "should not save without title" do
+
+  test "booking should not save without title" do
     booking = Booking.new(start_time:"2020-07-20 19:00:00", end_time:"2020-07-20 20:00:00", room_id:1, user_id:1)
     assert_not booking.save, "Saved the booking without a title"
   end
 
-  test "should not overlap with other bookings" do
-    print @bookings['booking1']['start_time']
-    booking = Booking.new(bookings[:one].start_time, bookings[:one].end_time, bookings[:one].room_id, bookings[:one].user_id,bookings[:one].title)
-    assert_not booking.save, "Create the booking with overlap bookings"
+  test "booking should not overlap with other bookings" do
+    booking = Booking.new(start_time: bookings(:booking1).start_time, end_time: bookings(:booking1).end_time, room_id: bookings(:booking1).room_id, user_id: bookings(:booking1).user_id,title: bookings(:booking1).title)
+    assert_not booking.save, "Create booking that overlap with otherbookings"
   end
+
+  test "booking date to only be within a day" do
+    booking = Booking.new(start_time: "2020-07-20 19:00:00", end_time: "2020-07-21 20:00:00", room_id: bookings(:booking1).room_id, user_id: bookings(:booking1).user_id,title: bookings(:booking1).title)
+    assert_not booking.save, "Create booking across multiple days"
+  end
+
+  test "booking end datetime should not be lesser than start datetime" do
+    booking = Booking.new(start_time: "2020-07-20 19:00:00", end_time: "2020-07-21 20:00:00", room_id: bookings(:booking1).room_id, user_id: bookings(:booking1).user_id,title: bookings(:booking1).title)
+    assert_not booking.save, "Create booking with end datetime higher than start datetime"
+  end
+
 end
