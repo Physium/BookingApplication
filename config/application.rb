61b306a81aa307ee53d5ef8115db594eb3a4a882
Loh Wei Jun <wjloh91@gmail.com>
2021-02-24 03:15:02 +0800

    Update bundle packages. Add dotenv gem to manage env variables.

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module GovtechApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
