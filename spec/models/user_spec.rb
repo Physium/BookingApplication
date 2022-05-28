6abd9f201afc7fd7c46bd9e557f62955694ea862
Loh Wei Jun <wjloh91@gmail.com>
2022-05-29 01:13:15 +0800

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:bookings) }
+    it { is_expected.to have_many(:rooms).through(:bookings) }
+  end
+
+  describe 'validations' do
+    context 'basic validations' do
+      it do
+        is_expected.to validate_presence_of(:first_name)
+        is_expected.to validate_presence_of(:last_name)
+        is_expected.to validate_presence_of(:username)
+        is_expected.to validate_uniqueness_of(:username).case_insensitive
+        is_expected.to validate_length_of(:username).is_at_least(3).is_at_most(25)
+        is_expected.to validate_presence_of(:email)
+        is_expected.to validate_length_of(:email).is_at_most(105)
+        is_expected.to allow_value('test@gmail.com').for(:email)
+        is_expected.to_not allow_value('invalidemail').for(:email)
+        is_expected.to have_secure_password(:password)
+      end
+    end
+  end
 end
