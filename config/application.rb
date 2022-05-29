308f82731d275e9b7a10f943d281172632ba8a94
Loh Wei Jun <wjloh91@gmail.com>
2022-05-29 19:22:11 +0800

    Update timezone config to Singapore

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module GovtechApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Singapore'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
